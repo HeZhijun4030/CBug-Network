92a93cc1229b54e1b6e000c5b244cd983afca9fc
HeZhijun4030 <Zhijun4030@outlook.com>
2025-06-01 13:23:10 +0800

    优化目录结构

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import socket
-import UDP_Func
+import UDP_Func.core as us
 
 # 测试代码
 if __name__ == "__main__":
-    sender = UDP_Func.udp_Sender()
+    sender = us.udp_Sender()
     sender.ip = "192.168.1.7"
     sender.port = 9999
 
-    sender.UDP_sendto(b"Hello World", expect_reply=True)
+    sender.send(b"Hello World", expect_reply=True)
 
